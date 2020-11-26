57d1ef4145d559b11fed4e007e8fa55b076ef72f
Thomas Shamp <T.shamp7@gmail.com>
2020-11-25 21:04:30 -0800

    add conditional rendering for geolocation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./components/SeasonDisplay";
 
-const App = () => {
-  return <div>Hello World!</div>;
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lat: null,
+      errorMessage: "",
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  // React says render mus be defined.
+  render() {
+    if (!this.state.lat && this.state.errorMessage) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    } else if (this.state.lat && !this.state.errorMessage) {
+      return <div>Latitude: {this.state.lat}</div>;
+    } else if (!this.state.lat && !this.state.errorMessage) {
+      return <div>Loading...</div>;
+    }
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
