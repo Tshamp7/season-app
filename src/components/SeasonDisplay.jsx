ba1aa25d1dd7609348efe7942f8be9d997cad930
Thomas Shamp <T.shamp7@gmail.com>
2020-11-28 22:15:19 -0800

    add logic to display season specific text

--- src/components/SeasonDisplay.jsx
+++ src/components/SeasonDisplay.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 
 const getSeason = (lat, month) => {
-  const latSouthHem = lat <= 0 && lat != null;
-  const latNorthHem = lat >= 0 && lat != null;
-
-  if (latSouthHem && ![3, 4, 5, 6, 7, 8].includes(month)) {
-    return "Summer";
-  } else if (latSouthHem && [3, 4, 5, 6, 7, 8].includes(month)) {
-    return "Winter";
-  }
-
-  if (latNorthHem && ![3, 4, 5, 6, 7, 8].includes(month)) {
-    return "Winter";
-  } else if (latNorthHem && [3, 4, 5, 6, 7, 8].includes(month)) {
-    return "Summer";
+  if (![3, 4, 5, 6, 7, 8].includes(month)) {
+    return lat > 0 ? "winter" : "summer";
+  } else {
+    return lat > 0 ? "summer" : "winter";
   }
 };
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
+  const text =
+    season === "winter" ? "Brrr, it's chilly!" : "Let's hit the beach!";
 
-  return <div>{season}</div>;
+  const icon = season === "winter" ? "snowflake" : "sun";
+
+  return (
+    <div>
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
